23d51a17e8e086901b4d041be8151fad194b5ea2
Mon8910 <107960871+Mon8910@users.noreply.github.com>
2022-08-17 17:26:34 +0200

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+class Myclass :
+    def __init__(self):
+        print("the class is very good")
+    def __del__(self):
+        print("the class is deleted")
+t1=Myclass()
+del t1
+
+
+# In[ ]:
+
+
+
+
