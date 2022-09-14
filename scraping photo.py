34365d21aeeb90e004c0e438c1e9aa5938072f8d
Mon8910 <107960871+Mon8910@users.noreply.github.com>
2022-09-15 00:14:14 +0200

    Add files via upload

---
+++ scraping photo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+
+# In[3]:
+
+
+website=requests.get("https://coreyms.com/page/3").text
+website
+
+
+# In[5]:
+
+
+soup=BeautifulSoup(website,'lxml')
+print(soup.prettify())
+
+
+# In[10]:
+
+
+csvfile=open('scrape88.csv','w',encoding='utf-8')
+writer=csv.writer(csvfile)
+writer.writerow(['head','paragragh'])
+
+
+# In[15]:
+
+
+articles=soup.find_all("article")
+for i in articles :
+    head=i.header.h2.a.text
+    print(head)
+    paragragh=i.find('div',class_="entry-content").p.text
+    print(paragragh)
+    writer.writerow([ head,paragragh])
+csvfile.close()    
+
+
+# In[16]:
+
+
+art=soup.find('img',class_='rg_i Q4LuWd')
+print(art)
+
+
+# In[ ]:
+
+
+
+
